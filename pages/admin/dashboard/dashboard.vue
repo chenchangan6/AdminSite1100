5ae15ff7d4aa50b4a4ffe1741ad61951962ffdec
chenchangan6 <chenchangan6@gmail.com>
2019-02-03 21:19:51 +0800

    后台

--- pages/admin/dashboard/dashboard.vue
+++ pages/admin/dashboard/dashboard.vue
@@ -6,7 +6,11 @@
           <img src="@/assets/images/logo/logo-1.png" />
         </div>
         <div class="main_menu">
-          <el-menu name='mainMenues' unique-opened background-color="transparent" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu name='mainMenues'
+                   unique-opened
+                   background-color="transparent"
+                   text-color="#fff"
+                   active-text-color="#ffd04b">
             <label v-for="x,index in MainMenu">
               <!-- 导航类 -->
               <label v-if="x.mode==='1'">
@@ -40,8 +44,21 @@
 
     </el-aside>
     <el-container>
-      <el-header>Header</el-header>
-      <el-main>Main</el-main>
+      <el-header class="main_header">
+        <el-row class="cor">
+          <el-cor :span="2">
+            <i class="icon font_family icon-shouqi"></i>
+          </el-cor>
+          <el-cor :span="2">
+            <div>1</div>
+          </el-cor>
+          <el-cor :span="2">2</el-cor>
+          <el-cor :span="2">3</el-cor>
+          <el-cor :span="2">4</el-cor>
+        </el-row>
+
+      </el-header>
+      <el-main class="main_main">Main</el-main>
     </el-container>
   </el-container>
 
@@ -49,11 +66,190 @@
 
 <script>
 export default {
-    data(){
-return{
-    MainMenu:[{"MenuType":"Top","parentid":"0","title":"工作台","number":"1","icon":"icon-gongzuotai","mode":"2"},{"MenuType":"Top","title":"课程管理","number":"1","icon":"icon-kechengguanli","mode":"1","children":[{"MenuType":"Child","parentid":"1","title":"课程列表","number":"1","icon":"icon-kechengguanli","mode":"2"},{"MenuType":"Child","parentid":"1","title":"课程分类","number":"1","icon":"icon-kechengguanli","mode":"2"}]},{"MenuType":"Top","title":"订单管理","number":"1","icon":"icon-dingdanguanli","mode":"1","children":[{"MenuType":"Child","parentid":"2","title":"购买订单","number":"1","icon":"icon-dingdanguanli","mode":"2"},{"MenuType":"Child","parentid":"2","title":"邀请订单","number":"1","icon":"icon-dingdanguanli","mode":"2"},{"MenuType":"Child","parentid":"2","title":"积分兑换","number":"1","icon":"icon-dingdanguanli","mode":"2"},{"MenuType":"Child","parentid":"2","title":"实物订单","number":"1","icon":"icon-dingdanguanli","mode":"2"}]},{"MenuType":"Top","parentid":"1","title":"打卡管理","number":"1","icon":"icon-daqiaguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"用户管理","number":"1","icon":"icon-yonghuguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"讲师管理","number":"1","icon":"icon-jiangshiguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"班级管理","number":"1","icon":"icon-banjiguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"视频管理","number":"1","icon":"icon-shipinguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"评价管理","number":"1","icon":"icon-pingjiaguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"消息管理","number":"1","icon":"icon-xiaoxiguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"积分管理","number":"1","icon":"icon-jifenguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"兑换码管理","number":"1","icon":"icon-duihuanmaguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"反馈管理","number":"1","icon":"icon-fankuiguanli","mode":"2"},{"MenuType":"Top","parentid":"1","title":"基本设置","number":"1","icon":"icon-jifenshezhi","mode":"2"},{"MenuType":"Top","parentid":"1","title":"管理员","number":"1","icon":"icon-guanliyuan","mode":"2"},{"MenuType":"Top","parentid":"1","title":"数据统计","number":"1","icon":"icon-shujutongji","mode":"2"}]
-}
-    },
+  data() {
+    return {
+      MainMenu: [
+        {
+          MenuType: "Top",
+          parentid: "0",
+          title: "工作台",
+          number: "1",
+          icon: "icon-gongzuotai",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          title: "课程管理",
+          number: "1",
+          icon: "icon-kechengguanli",
+          mode: "1",
+          children: [
+            {
+              MenuType: "Child",
+              parentid: "1",
+              title: "课程列表",
+              number: "1",
+              icon: "icon-kechengguanli",
+              mode: "2"
+            },
+            {
+              MenuType: "Child",
+              parentid: "1",
+              title: "课程分类",
+              number: "1",
+              icon: "icon-kechengguanli",
+              mode: "2"
+            }
+          ]
+        },
+        {
+          MenuType: "Top",
+          title: "订单管理",
+          number: "1",
+          icon: "icon-dingdanguanli",
+          mode: "1",
+          children: [
+            {
+              MenuType: "Child",
+              parentid: "2",
+              title: "购买订单",
+              number: "1",
+              icon: "icon-dingdanguanli",
+              mode: "2"
+            },
+            {
+              MenuType: "Child",
+              parentid: "2",
+              title: "邀请订单",
+              number: "1",
+              icon: "icon-dingdanguanli",
+              mode: "2"
+            },
+            {
+              MenuType: "Child",
+              parentid: "2",
+              title: "积分兑换",
+              number: "1",
+              icon: "icon-dingdanguanli",
+              mode: "2"
+            },
+            {
+              MenuType: "Child",
+              parentid: "2",
+              title: "实物订单",
+              number: "1",
+              icon: "icon-dingdanguanli",
+              mode: "2"
+            }
+          ]
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "打卡管理",
+          number: "1",
+          icon: "icon-daqiaguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "用户管理",
+          number: "1",
+          icon: "icon-yonghuguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "讲师管理",
+          number: "1",
+          icon: "icon-jiangshiguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "班级管理",
+          number: "1",
+          icon: "icon-banjiguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "视频管理",
+          number: "1",
+          icon: "icon-shipinguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "评价管理",
+          number: "1",
+          icon: "icon-pingjiaguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "消息管理",
+          number: "1",
+          icon: "icon-xiaoxiguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "积分管理",
+          number: "1",
+          icon: "icon-jifenguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "兑换码管理",
+          number: "1",
+          icon: "icon-duihuanmaguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "反馈管理",
+          number: "1",
+          icon: "icon-fankuiguanli",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "基本设置",
+          number: "1",
+          icon: "icon-jifenshezhi",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "管理员",
+          number: "1",
+          icon: "icon-guanliyuan",
+          mode: "2"
+        },
+        {
+          MenuType: "Top",
+          parentid: "1",
+          title: "数据统计",
+          number: "1",
+          icon: "icon-shujutongji",
+          mode: "2"
+        }
+      ]
+    };
+  },
   layout: "admin"
 };
 </script>
